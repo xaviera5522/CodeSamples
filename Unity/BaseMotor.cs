f06462efd70d12ba27d01a87dca7e3afdd6b4553
Alex Xavier <xaviera5522@gmail.com>
2020-03-09 20:33:19 -0700

    Adding BaseMotor.cs & CharacterMotor.cs

---
+++ Unity/BaseMotor.cs
@@ -0,0 +1,290 @@
+﻿//This class is a motor for an action Platformer.
+//This class is not expected to be used on its own. This is a parent class for all movent motors
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMotor : MonoBehaviour
+{
+    public Del dashEnded;	//A delegate that fires at the end of a dash. this is mostly used for combos
+    public PublicEnums.Direction direction;	//The Direction this character if facing
+
+    public float maxSpeed;
+    public float jumpForce;
+    protected int jumpCount;
+    public int maxJumps;
+
+    public float inAirSpeed;
+    public float inAirAccel;
+
+    protected float initJumpForce;
+
+    public bool onGround;
+
+    public delegate void Del();
+
+    protected Vector2 currentSpeed;
+
+    [HideInInspector]
+    public float height;
+
+    public bool stun;
+
+    public float gravity = 10f;
+    float initGravity;
+
+    protected Rigidbody2D rigid;
+
+    public Character character;
+
+    protected PublicEnums.CurrentMovement currentMovement;
+
+    public BaseController controller;
+
+    public float dashDist, upperCutDist;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        initGravity = gravity;
+        direction = PublicEnums.Direction.Right;
+
+        initJumpForce = jumpForce;
+        height = transform.lossyScale.y;
+        stun = false;
+
+        rigid = GetComponent<Rigidbody2D>();
+        currentMovement = PublicEnums.CurrentMovement.None;
+        if (!controller)
+            controller = GetComponent<BaseController>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!stun && currentMovement == PublicEnums.CurrentMovement.None)
+        {
+            rigid.MovePosition(Vector2.Lerp(rigid.position,
+                rigid.position + currentSpeed * Time.fixedDeltaTime, maxSpeed));
+        }
+
+        if (!onGround)
+        {
+            if (currentSpeed.x > 0)
+            {
+                currentSpeed.x -= inAirAccel;
+                if (currentSpeed.x <= 0.25f)
+                    currentSpeed.x = 0;
+            }
+            else if (currentSpeed.x < 0)
+            {
+                currentSpeed.x += inAirAccel;
+                if (currentSpeed.x >= -0.25f)
+                    currentSpeed.x = 0;
+            }
+        }
+    }
+
+    public virtual void Move(float multiplier)
+    {
+        \\Do Nothing
+    }
+    public virtual void MoveLeft(float multiplier)
+    {
+        \\Do Nothing
+    }
+    public virtual void Move()
+    {
+        currentSpeed = maxSpeed * PublicEnums.DirectionToVector2(direction);
+    }
+    public void MoveLeft()
+    {
+        MoveLeft(1);
+    }
+    public virtual void MoveLeftInAir(float multiplier)
+    {
+        \\Do Nothing
+    }
+    public void MoveLeftInAir()
+    {
+        MoveLeftInAir(1);
+    }
+    public virtual void MoveRight(float multiplier)
+    {
+        \\Do Nothing
+    }
+    public void MoveRight()
+    {
+        MoveRight(1);
+    }
+    public virtual void MoveRightInAir(float multiplier)
+    {
+        \\Do Nothing
+    }
+    public void MoveRightInAir()
+    {
+        MoveRightInAir(1);
+    }
+    public virtual void MoveInAir(float multiplier)
+    {
+        \\Do Nothing
+    }
+    public virtual void Jump()
+    {
+        \\Do Nothing
+    }
+
+    public virtual void Dash(PublicEnums.Direction dir, float distance)
+    {
+        gravity *= 0f;
+        dashEnded += ResetGravity;
+    }
+    public virtual void Dash(float distance)
+    {
+        Dash(direction, distance);
+        gravity *= 0f;
+        dashEnded += ResetGravity;
+    }
+    public virtual void DiveKick()
+    {
+        
+    }
+    public virtual void Step()
+    {
+        StartCoroutine("StepAction");
+    }
+    public virtual void Charge()
+    {
+
+    }
+
+    public IEnumerator StepAction()
+    {
+        SetCurrentMovement(PublicEnums.CurrentMovement.Step);
+        Vector2 startPos = rigid.position;
+
+        Vector2 dir = direction == PublicEnums.Direction.Right ? Vector2.right : Vector2.left; 
+
+        while(Vector2.Distance(rigid.position, startPos) < 0.5)
+        {
+            rigid.position += dir * maxSpeed * Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+
+        startPos = rigid.position;
+        dir = -dir;
+        while (Vector2.Distance(rigid.position, startPos) < 0.25)
+        {
+            rigid.position += dir * maxSpeed * Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        currentMovement = (PublicEnums.CurrentMovement.None);
+        controller.ReturnControl();
+    }
+
+    public void Stun(float duration)
+    {
+        if (stun)
+        {
+            CancelInvoke("EndStun");
+        }
+        stun = true;
+        StopMomentum();
+        Invoke("EndStun", duration);
+    }
+
+    public bool Higher(GameObject o) //returns true when this character is higer than o
+    {
+        return o.transform.position.y < (transform.position.y - (height * 0.5f));
+    }
+    public bool Lower(GameObject o) //returns true wheh this character is lower than o
+    {
+        return o.transform.position.y > (transform.position.y + (height * 0.5f));
+    }
+
+    public void EndStun()
+    {
+        currentSpeed.x = 0;
+        stun = false;
+    }
+
+    public void StopMomentum()	//Stop Horizontal Movement
+    {
+        currentSpeed.x = 0;
+    }
+    public void FullStopMomentum()	//stop all movement
+    {
+        currentSpeed = Vector2.zero;
+    }
+    public void SetOnGround(bool grounded)//Changes state from in air to on ground and vice versa
+    {
+        onGround = grounded;
+        if (onGround)
+        {
+            currentSpeed.y = 0;//Stops the gravity vector
+
+            if(currentMovement == PublicEnums.CurrentMovement.DiveKick)
+            {
+                StopCurrentMovement();	//End DiveKick to avoid weird sliding
+            }
+
+            ResetJumps();//Resets ability to double and tripple jump
+        }
+    }
+    public void ResetJumps()
+    {
+        jumpCount = 0;//Now they can jump again
+        jumpForce = initJumpForce;//sets jumpForce to the correct value 
+    }
+    protected void SetCurrentMovement(PublicEnums.CurrentMovement movement)
+    {
+        if(currentMovement != PublicEnums.CurrentMovement.None || 
+            movement == PublicEnums.CurrentMovement.None)
+        {
+            StopCurrentMovement();//If the current movement is None then stop whatever was happening
+        }
+
+        currentMovement = movement;
+        FullStopMomentum();//Full stop so there aren't any surprises with the vectors adding together.
+    }
+    public void StopCurrentMovement()
+    {
+        switch (currentMovement)
+        {
+            case PublicEnums.CurrentMovement.DiveKick:
+                StopCoroutine("DiveDash");
+                break;
+            case PublicEnums.CurrentMovement.DashLeft:
+                StopCoroutine("DashLeft");
+                break;
+            case PublicEnums.CurrentMovement.DashRight:
+                StopCoroutine("DashRight");
+                break;
+            case PublicEnums.CurrentMovement.DashUp:
+                StopCoroutine("DashUp");
+                break;
+            case PublicEnums.CurrentMovement.DashDown:
+                StopCoroutine("DashDown");
+                break;
+            case PublicEnums.CurrentMovement.Step:
+                StopCoroutine("StepAction");
+                break;
+            case PublicEnums.CurrentMovement.Charge:
+                StopCoroutine("ChargeMove");
+                break;
+            default:
+                break;
+        }
+        currentMovement = PublicEnums.CurrentMovement.None;	//reset currentMovement to None
+        controller.ReturnControl();	//Some actions may remove control. This line is a failsafe.
+    }
+    protected void ResetGravity()	//Sets gravity to the value it was when this object was initiated
+    {
+        gravity = initGravity;
+    }
+}
---
+++ Unity/CharacterMotor.cs
@@ -0,0 +1,334 @@
+﻿//This class is a motor for an action Platformer. 
+//The movement handled in this class and BaseMotor are designed for a 2D character 
+//that can be either the player or an enemy that uses Dashes as a primary form of movement
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotor : BaseMotor
+{
+    public float dashSpeed;
+    //public float dashDist = 5;//moved to the Dash function
+
+    public float diveKickDist = 5;
+    public float diveKickSpeed = 10;
+
+    public float upperCutSpeed = 10; //up Dash attack
+    //public float upperCutDist = 5;//moved to the Dash function
+
+    PublicEnums.Direction wallDirection; //relative direction to the wall the player is sliding on
+    public bool sliding { get; set; }
+    public float slideFallMultiplier = 0.33f;	//wall slide gravity reduction
+    public float slideJumpMultiplier = 0.5f;	//wall jump reduction
+
+    // Use this for initialization
+    void Start ()
+    {
+        direction = PublicEnums.Direction.Right;	//Player character starts facing right (for now)
+
+        initJumpForce = jumpForce;
+        height = transform.lossyScale.y;
+        stun = false;
+
+        rigid = GetComponent<Rigidbody2D>();	//Get the Rigidbody
+        sliding = false;
+
+        ResetDiveEnd();	//clear dashEnded delegate
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		if(!stun && currentMovement == PublicEnums.CurrentMovement.None)
+        {
+            rigid.MovePosition(Vector2.Lerp(rigid.position, 
+                rigid.position + currentSpeed * Time.fixedDeltaTime, maxSpeed));
+        }
+
+        if (!onGround)
+        {
+            if (currentSpeed.x > 0)
+            {
+                currentSpeed.x -= inAirAccel;
+                if (currentSpeed.x <= 0.25f)
+                    currentSpeed.x = 0;
+            }
+            else if (currentSpeed.x < 0)
+            {
+                currentSpeed.x += inAirAccel;
+                if (currentSpeed.x >= -0.25f)
+                    currentSpeed.x = 0;
+            }
+            if (!sliding)
+            {
+                currentSpeed.y -= gravity * Time.fixedDeltaTime;
+            }
+            else
+            {
+                currentSpeed.y -= gravity * slideFallMultiplier * Time.fixedDeltaTime;
+            }
+        }
+    }
+
+    public override void Move(float multiplier)//Multiplier is for movement with a thumbstick
+    {
+        multiplier = Mathf.Clamp(multiplier, -1, 1);//just in case the multiplier goes out of bounds
+        if(multiplier < 0)
+        {
+            direction = PublicEnums.Direction.Left;
+        }
+        else if(multiplier > 0)
+        {
+            direction = PublicEnums.Direction.Right;
+        }
+        if (!sliding || (sliding && direction != wallDirection))//sliding is checked by a collider
+            currentSpeed.x = maxSpeed * multiplier;//move normally if the movement direction is oppisite of the sliding dirction
+    }
+    public override void MoveLeft(float multiplier)
+    {
+        direction = PublicEnums.Direction.Left;
+        multiplier = Mathf.Clamp(multiplier, 0, 1);
+        if (!sliding || (sliding && direction != wallDirection))
+            currentSpeed.x = -maxSpeed * multiplier;
+    }
+    public override void MoveLeftInAir(float multiplier)
+    {
+        multiplier = Mathf.Clamp(multiplier, 0, 1);
+        currentSpeed.x -= multiplier * inAirAccel;
+        direction = PublicEnums.Direction.Left;
+        if (!sliding || (sliding && direction != wallDirection))
+            currentSpeed.x = Mathf.Clamp(currentSpeed.x, -inAirSpeed, inAirSpeed);
+    }
+    public override void MoveRight(float multiplier)
+    {
+        direction = PublicEnums.Direction.Right;
+        multiplier = Mathf.Clamp(multiplier, 0, 1);
+        if (!sliding || (sliding && direction != wallDirection))
+            currentSpeed.x = maxSpeed * multiplier;
+    }
+    public override void MoveRightInAir(float multiplier)
+    {
+        multiplier = Mathf.Clamp(multiplier, 0, 1);
+        currentSpeed.x = multiplier * inAirAccel;
+        direction = PublicEnums.Direction.Right;
+        if (!sliding || (sliding && direction != wallDirection))
+            currentSpeed.x = Mathf.Clamp(currentSpeed.x, -inAirSpeed, inAirSpeed);
+    }
+    public override void MoveInAir(float multiplier)
+    {
+        multiplier = Mathf.Clamp(multiplier, -1, 1);
+        if (multiplier < 0)
+        {
+            direction = PublicEnums.Direction.Left;
+        }
+        else
+        {
+            direction = PublicEnums.Direction.Right;
+        }
+        if (!sliding || (sliding && direction != wallDirection))
+            currentSpeed.x += inAirAccel * multiplier;
+
+        currentSpeed.x = Mathf.Clamp(currentSpeed.x, -inAirSpeed, inAirSpeed);
+    }
+    public override void Jump()
+    {
+        if (jumpCount < maxJumps)
+        {
+            if(sliding)
+            {
+                currentSpeed.y = jumpForce * slideJumpMultiplier;
+                float xForce = jumpForce / slideJumpMultiplier;
+                currentSpeed.x = direction == PublicEnums.Direction.Right ? -xForce : xForce;
+            }
+            else
+            {
+                currentSpeed.y = jumpForce;
+                currentSpeed.x *= 0.75f; //TODO: Also lose a lot of x-speed when the player jumps
+            }
+
+            if (jumpCount != 0)
+                jumpForce = initJumpForce / ((float)jumpCount + 0.25f);
+
+            jumpCount++;
+        }
+    }
+    public IEnumerator DashLeft(float distance)
+    {
+        currentMovement = (PublicEnums.CurrentMovement.DashLeft);
+
+        float dist = 0;
+        float delta = 0;
+
+        direction = PublicEnums.Direction.Left;
+
+        while (dist < distance)
+        {
+            delta = dashSpeed * Time.fixedDeltaTime;
+            dist += delta;
+
+            rigid.position += Vector2.left * delta;
+            yield return new WaitForFixedUpdate();
+        }
+        if (dashEnded != null)
+        {
+            dashEnded();
+            dashEnded = null;
+        }
+        SetCurrentMovement(PublicEnums.CurrentMovement.None);
+    }
+    public IEnumerator DashRight(float distance)
+    {
+        SetCurrentMovement(PublicEnums.CurrentMovement.DashRight);
+
+        float dist = 0;
+        float delta = 0;
+
+        direction = PublicEnums.Direction.Right;
+
+        while (dist < distance)
+        {
+            delta = dashSpeed * Time.fixedDeltaTime;
+            dist += delta;
+            rigid.position += Vector2.right * delta;
+            yield return new WaitForFixedUpdate();
+        }
+        if (dashEnded != null)
+        {
+            dashEnded();
+            dashEnded = null;
+        }
+        SetCurrentMovement(PublicEnums.CurrentMovement.None);
+    }
+    public IEnumerator DashUp(float distance)
+    {
+        SetCurrentMovement(PublicEnums.CurrentMovement.DashUp);
+
+        float dist = 0;
+        float delta = 0; ;
+
+        while (dist < distance)
+        {
+            delta = dashSpeed * Time.fixedDeltaTime;
+            dist += delta;
+            rigid.position += Vector2.up * delta;
+            yield return new WaitForFixedUpdate();
+        }
+        if (dashEnded != null)
+        {
+            dashEnded();
+            dashEnded = null;
+        }
+        SetCurrentMovement(PublicEnums.CurrentMovement.None);
+    }
+    public IEnumerator DashDown(float distance)
+    {
+        SetCurrentMovement(PublicEnums.CurrentMovement.DashDown);
+
+        float dist = 0;
+        float delta = 0;
+
+        while (dist < distance)
+        {
+            delta = dashSpeed * Time.fixedDeltaTime;
+            dist += delta;
+            rigid.position += Vector2.down * delta;
+            yield return new WaitForFixedUpdate();
+        }
+        if (dashEnded != null)
+        {
+            dashEnded();
+            dashEnded = null;
+        }
+        SetCurrentMovement(PublicEnums.CurrentMovement.None);
+    }
+    public override void Dash(PublicEnums.Direction dir, float distance)
+    {
+        gravity *= 0.25f;
+        dashEnded += ResetGravity;
+        switch (dir)
+        {
+            case PublicEnums.Direction.Left:
+                StartCoroutine(DashLeft(distance));
+                break;
+            case PublicEnums.Direction.Right:
+                StartCoroutine(DashRight(distance));
+                break;
+            case PublicEnums.Direction.Up:
+                StartCoroutine(DashUp(distance));
+                break;
+            case PublicEnums.Direction.Down:
+                StartCoroutine(DashDown(distance));
+                break;
+            default:
+                break;
+        }
+    }
+    public void WallSlide() //CUT
+    {
+        //currentSpeed = Vector2.zero;//Could be a quick decceleration
+        //wallDirection = direction;
+        //sliding = true;
+    }
+    public void WallSlide(PublicEnums.Direction dir) //CUT
+    {
+        //currentSpeed = Vector2.zero;//Could be a quick decceleration
+        //wallDirection = dir;
+        //sliding = true;
+    }
+    public override void DiveKick()
+    {
+        Vector2 dir;
+
+        switch (direction)
+        {
+            case PublicEnums.Direction.Left:
+                dir = new Vector2(-1f, -1f);
+                break;
+            case PublicEnums.Direction.Right:
+                dir = new Vector2(1f, -1f);
+                break;
+            default:
+                dir = new Vector2(0, -1f);
+                break;
+        }
+        StartCoroutine(DiveDash(dir.normalized));
+    }
+    public IEnumerator DiveDash(Vector2 dir)
+    {
+        SetCurrentMovement(PublicEnums.CurrentMovement.DiveKick);
+
+        float dist = 0;
+        float delta = 0;
+
+        while(dist < diveKickDist)
+        {
+            delta = diveKickSpeed * Time.fixedDeltaTime;
+            dist += delta;
+            rigid.position += dir * delta;
+            yield return new WaitForFixedUpdate();
+        }
+        if (dashEnded != null)
+        {
+            dashEnded();
+            dashEnded = null;
+        }
+        SetCurrentMovement(PublicEnums.CurrentMovement.None);
+    }
+
+    public void EndKick()
+    {
+        StopCoroutine("DiveDash");
+
+        if (dashEnded != null)
+        {
+            dashEnded();
+            dashEnded = null;
+        }
+        SetCurrentMovement(PublicEnums.CurrentMovement.None);
+    }
+    public void ResetDiveEnd()
+    {
+        dashEnded = null;
+    }
+}
